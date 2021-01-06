ea5f286f90a5524b4cdcbea3793a6d9287e3682b
eddiebrett <eddiebrett@hotmail.co.uk>
2021-01-06 23:09:51 +0000

    added checkout page

--- eddie_website/settings.py
+++ eddie_website/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+
+    #other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'eddie_website.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -71,8 +77,12 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
         },
     },
 ]
